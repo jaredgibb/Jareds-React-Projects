3fe0a0e028f9be893d7186eaaf88def920628d22
jaredgibb <70493366+jaredgibb@users.noreply.github.com>
2023-12-13 08:12:21 -0500

    moving files around. updating stuff

--- React Projects/Section 10 - Context API and useReducer - Advanced State Management/01-starting-project/src/components/Product.jsx
+++ React Projects/Section 10 - Context API and useReducer - Advanced State Management/01-starting-project/src/components/Product.jsx
@@ -1,11 +1,16 @@
+import { useContext } from 'react';
+import { CartContext } from '../store/CartContext.jsx';
+
 export default function Product({
   id,
   image,
   title,
   price,
   description,
-  onAddToCart,
 }) {
+
+  const { addItem } = useContext(CartContext);
+  
   return (
     <article className="product">
       <img src={image} alt={title} />
@@ -16,7 +21,7 @@ export default function Product({
           <p>{description}</p>
         </div>
         <p className='product-actions'>
-          <button onClick={() => onAddToCart(id)}>Add to Cart</button>
+          <button onClick={() => addItem(id)}>Add to Cart</button>
         </p>
       </div>
     </article>
